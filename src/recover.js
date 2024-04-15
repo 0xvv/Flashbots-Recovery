ee27ff748419f85c1693c1787bce8752eaba2c5f
0xvv <public_double_v@protonmail.com>
2024-04-15 16:46:17 +0200

    fix script

--- src/recover.js
+++ src/recover.js
@@ -3,12 +3,11 @@ import { FlashbotsBundleProvider } from '@flashbots/ethers-provider-bundle';
 import 'dotenv/config';
 
 const addresses = {
-  'gelatoToken': '0x15b7c0c907e4C6b9AdaAaabC300C08991D6CEA05',
-  'claimContract': '0x5898D2aE0745c8d09762Bac50fd9F34A2a95A563'
+  'exitQueue': '0x8d6fd650500f82c7d978a440348e5a9b886943bf',
 };
 
 const CHAIN_ID = 1;
-const provider = new ethers.providers.JsonRpcProvider(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`);
+const provider = new ethers.providers.JsonRpcProvider(`rpc.flashbots.net`);
 
 const compromised_wallet = new ethers.Wallet(process.env.COMPROMISED_WALLET, provider)
 const new_wallet = new ethers.Wallet(process.env.NEW_WALLET, provider)
@@ -29,39 +28,25 @@ async function recover() {
           transaction: {
             chainId: CHAIN_ID,
             to:compromised_wallet.address,
-            value: ethers.utils.parseEther('0.2'),
+            value: ethers.utils.parseEther('0.1'),
             type: 2,
             gasLimit: 21000,
-            maxFeePerGas: ethers.utils.parseUnits('120', 'gwei'),
-            maxPriorityFeePerGas: ethers.utils.parseUnits('110', 'gwei'),
+            maxFeePerGas: ethers.utils.parseUnits('60', 'gwei'),
+            maxPriorityFeePerGas: ethers.utils.parseUnits('70', 'gwei'),
           },
           signer: new_wallet 
         },
 
-        // transaction to claim ICO tokens
+        // transaction to claim ETH
         {
           transaction: {
             chainId: CHAIN_ID,
-            to: addresses.claimContract,
-            data: '0x704fc04e000000000000000000000000bc79c7139c87df965f0f4c24747f326d1864c5af',
+            to: addresses.exitQueue,
+            data: '0xadcf1163000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000196000000000000000001b9ada6122674540000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000b8',
             type: 2,
-            gasLimit: 91170,
-            maxFeePerGas: ethers.utils.parseUnits('120', 'gwei'),
-            maxPriorityFeePerGas: ethers.utils.parseUnits('110', 'gwei'),
-          },
-          signer: compromised_wallet
-        },
-
-        // transaction to withdraw
-        {
-          transaction: {
-            chainId: CHAIN_ID,
-            to: addresses.gelatoToken,
-            data: '0xa9059cbb000000000000000000000000acf6418cefd7254f5e34b2b2e9a8f081e0e150d1000000000000000000000000000000000000000000000845e16a00dd60f00000',
-            type: 2,
-            gasLimit: 78000,
-            maxFeePerGas: ethers.utils.parseUnits('120', 'gwei'),
-            maxPriorityFeePerGas: ethers.utils.parseUnits('110', 'gwei'),
+            gasLimit: 130000,
+            maxFeePerGas: ethers.utils.parseUnits('60', 'gwei'),
+            maxPriorityFeePerGas: ethers.utils.parseUnits('70', 'gwei'),
           },
           signer: compromised_wallet
         },
@@ -71,11 +56,11 @@ async function recover() {
           transaction: {
             chainId: CHAIN_ID,
             to: new_wallet.address,
-            value: new_wallet.getBalance() - ethers.utils.parseEther('0.00252'),
+            value: compromised_wallet.getBalance() - ethers.utils.parseEther('0.00252'),
             type: 2,
             gasLimit: 21000,
-            maxFeePerGas: ethers.utils.parseUnits('120', 'gwei'),
-            maxPriorityFeePerGas: ethers.utils.parseUnits('110', 'gwei'),
+            maxFeePerGas: ethers.utils.parseUnits('60', 'gwei'),
+            maxPriorityFeePerGas: ethers.utils.parseUnits('70', 'gwei'),
           },
           signer: compromised_wallet
         },
